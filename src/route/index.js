69e741d4a16d3e5fc29fbd880daa597305242951
unknown <makskanevski12@gmail.com>
2024-03-31 00:13:11 +0100

    dashboard

--- src/route/index.js
+++ src/route/index.js
@@ -1104,5 +1104,15 @@ router.get('/', function (req, res) {
 	  })
 	  //                  ↑↑ сюди вводимо JSON дані
 	}),
+
+	router.get('/dashboard', function (req, res) {
+		// res.render генерує нам HTML сторінку
+	
+		//            ↙ cюди вводимо назву файлу з сontainer
+		res.render('dashboard', {
+		  layout: null,
+		})
+		//                  ↑↑ сюди вводимо JSON дані
+	  }),
 	// Підключаємо роутер до бек-енду
 	module.exports = router
